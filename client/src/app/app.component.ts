c4b8a332ae97f0bc99391c8534ccaecf6f03b15f
flesnuk <flesnuk@gmail.com>
2018-06-17 22:14:10 +0200

    move filter to it's own component 2

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TicketService } from './ticket.service';
-import { Ticket } from './ticket';
 
 @Component({
 	selector: 'app-root',
@@ -9,36 +8,15 @@ import { Ticket } from './ticket';
 	providers: [TicketService]
 })
 export class AppComponent implements OnInit {
-	sessionNames: string[] = [];
 	session: string;
 
-	constructor(private ticketService: TicketService) {}
+	constructor() {}
 
-	ngOnInit() {
-		// load the sessionNames from the ticketService
-		this.ticketService
-			.fetch()
-			.subscribe(tickets => {
-				this.sessionNames = this.getSessionNames(tickets); },
-				err => {console.log(err);
-			});
-	}
+	ngOnInit() {}
 
 	setFilter(filter: string) {
 		// if the user clears the selection, filter will be undefined
 		// setting session to empty string means that we clear the filter
 		this.session = filter ? filter : '';
-	}
-
-	// get unique session names from tickets
-	private getSessionNames(tickets: Ticket[]) {
-		// Remove duplicated elements from a list of strings.
-		function unique(list) {
-			const seen = {};
-			return list.filter(function(item) {
-				return seen.hasOwnProperty(item) ? false : (seen[item] = true);
-			});
-		}
-		return unique(tickets.map(ticket => ticket.sessions[0].name));
 	}
 }
--- client/src/app/filter/filter.component.ts
+++ client/src/app/filter/filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { TicketService } from '../ticket.service';
+import { Ticket } from '../ticket';
 
 @Component({
 	selector: 'app-filter',
@@ -6,14 +8,34 @@ import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 	styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
-	@Input() sessionNames: string[] = [];
+	sessionNames: string[] = [];
 	@Output() filter = new EventEmitter<string>();
 
-	constructor() { }
+	constructor(private ticketService: TicketService) { }
 
-	ngOnInit() {}
+	ngOnInit() {
+		// load the sessionNames from the ticketService
+		this.ticketService
+			.fetch()
+			.subscribe(tickets => {
+				this.sessionNames = this.getSessionNames(tickets); },
+				err => {console.log(err);
+			});
+	}
 
 	onSelect(filter: string) {
 		this.filter.emit(filter);
+	}
+
+	// get unique session names from tickets
+	private getSessionNames(tickets: Ticket[]) {
+		// Remove duplicated elements from a list of strings.
+		function unique(list) {
+			const seen = {};
+			return list.filter(function(item) {
+				return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+			});
+		}
+		return unique(tickets.map(ticket => ticket.sessions[0].name));
 	}
 }
